c1973a377000a7a3bb6f275ad0a7cdb4eede2fa2
creamytoad <mathias.lervik@hotmail.com>
2021-02-10 11:17:23 +0100

    added clock

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Dadjokes from "./dadjokes.js";
 import Bikes from "./bikes.js";
+import Clock from "./clock.js";
 
 function App() {
   return (
     <div className="App">
 
+        <Clock />
         <Dadjokes />
         <Bikes />
 
